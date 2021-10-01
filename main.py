1f0e671994bb1a56af7615973f60f172f67ed1a6
Zaharie Stefan <stefan@zaha.tech>
2021-10-01 19:27:17 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+def get_base_16_from_2():
+    pass
+
+def test_get_base_16_from_2():
+    pass
+
+def get_n_choose_k():
+    pass
+
+def test_get_n_choose_k():
+    pass
+
+def main():
+    pass
+
+if __name__ == __main__:
+    main()
