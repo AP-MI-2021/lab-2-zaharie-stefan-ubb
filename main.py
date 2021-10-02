4f7f992930ca669e3bd2cf1110da99d046bcaab2
Zaharie Stefan <stefan@zaha.tech>
2021-10-02 09:19:49 +0300

    Function get_base_16_from_2

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 from math import factorial
 
 
-def get_base_16_from_2():
-    pass
+def get_base_16_from_2(num_in_base_2):
+    """
+    Primeste un numar in baza 2 si returneaza acelasi numar in baza 16
+
+    Args:
+    num_in_base_2 -> str
+
+    Returns: str
+    """
+
+    # Verifica daca numarul introdus este corect
+    for c in num_in_base_2:
+        if (c != "0") and (c != "1"):
+            return "Eroare! Numarul introdus este incorect"
+
+    # Caz particular
+    if num_in_base_2 == "0":
+        return "0"
+
+    num_in_base_10 = int(num_in_base_2, 2)
+    remainder_stack = []
+    while num_in_base_10 > 0:
+        remainder_stack.append(num_in_base_10 % 16)
+        num_in_base_10 //= 16
+
+    digits = "0123456789ABCDEF"
+    num_in_base_16 = []
+    while remainder_stack:
+        num_in_base_16.append(digits[remainder_stack.pop()])
+    return "".join(num_in_base_16)
 
 
 def test_get_base_16_from_2():
